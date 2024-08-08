f89d2b1efcd8c1e10bc4f554b654f3e4d436fb84
Almaz Dautov <64196918+thehir0@users.noreply.github.com>
2024-08-09 01:21:15 +0300

    vllm support

--- evaluation/model_loader.py
+++ evaluation/model_loader.py
@@ -2,12 +2,14 @@ import torch
 import transformers
 
 from transformers import LlamaTokenizer, AutoModelForCausalLM, AutoTokenizer, AutoConfig
-
+from vllm import LLM
 
 class ModelLoader:
-    def __init__(self, model_path, model_torch_dtype, tokenizer_path=None, device="cpu"):
+    def __init__(self, model_path, model_torch_dtype, tokenizer_path=None, engine="hf", tensor_parallel_size=1, device="cpu"):
         self.model_path = model_path
+        self.engine = engine
         self.device = device
+        self.tensor_parallel_size=tensor_parallel_size
         self.model_torch_dtype = self.get_dtype(model_torch_dtype)
         if tokenizer_path:
             self.tokenizer_path = tokenizer_path
@@ -26,21 +28,28 @@ class ModelLoader:
             return torch.float
 
     def model_load(self):
-        config = transformers.AutoConfig.from_pretrained(self.model_path)
-        if "LlamaForCausalLM" in config.architectures:
-            model = AutoModelForCausalLM.from_pretrained(
-                self.model_path,
-                device_map=self.device,
-                attn_implementation='flash_attention_2',
-                trust_remote_code=True,
-                torch_dtype=self.model_torch_dtype,
-            ).eval()
+        if self.engine == 'hf':
+            config = transformers.AutoConfig.from_pretrained(self.model_path)
+            if "LlamaForCausalLM" in config.architectures:
+                model = AutoModelForCausalLM.from_pretrained(
+                    self.model_path,
+                    device_map=self.device,
+                    attn_implementation='flash_attention_2',
+                    trust_remote_code=True,
+                    torch_dtype=self.model_torch_dtype,
+                ).eval()
+            else:
+                model = AutoModelForCausalLM.from_pretrained(
+                    self.model_path,
+                    device_map=self.device,
+                    trust_remote_code=True,
+                    torch_dtype=self.model_torch_dtype,
+                ).eval()
+            tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_path, trust_remote_code=True)
+        elif self.engine == 'vllm':
+            model = LLM(model=self.model_path, tensor_parallel_size=self.tensor_parallel_size, dtype=self.model_torch_dtype, trust_remote_code=True)
+            tokenizer = model.get_tokenizer()
         else:
-            model = AutoModelForCausalLM.from_pretrained(
-                self.model_path,
-                device_map=self.device,
-                trust_remote_code=True,
-                torch_dtype=self.model_torch_dtype,
-            ).eval()
-        tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_path, trust_remote_code=True)
+            raise Exception('Engine should be \"hf\" or \"vllm\"')
+
         return model, tokenizer
