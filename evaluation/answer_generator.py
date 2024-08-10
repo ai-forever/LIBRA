887a7bca557735cbdfd7ac61f0ed51fcc90d605c
Almaz Dautov <64196918+thehir0@users.noreply.github.com>
2024-08-10 23:31:32 +0300

    refactor and batching

--- evaluation/answer_generator.py
+++ evaluation/answer_generator.py
@@ -2,9 +2,8 @@ from vllm import SamplingParams
 from tqdm import tqdm
 
 class AnswerGenerator:
-    def __init__(self, model, engine, tokenizer, device, dataset, instruction, context_lengths, max_context_length, max_new_tokens):
+    def __init__(self, model, tokenizer, device, dataset, instruction, context_lengths, max_context_length, max_new_tokens):
         self.model = model
-        self.engine = engine
         self.tokenizer = tokenizer
         self.dataset = dataset
         self.instruction = instruction
@@ -17,35 +16,50 @@ class AnswerGenerator:
         prompt = self.instruction
         prompt = prompt.replace("{context}", sample["context"])
         prompt = prompt.replace("{input}", sample["input"])
-        return prompt      
+        sample['prompt'] = prompt
+        return sample
         
     def generate_answers(self):
         generated_answers = []
         for sample in tqdm(self.dataset):
             if sample["length"] not in self.context_lengths: continue
             prompt = self.create_prompt(sample)
-
-            if self.engine == 'hf':
-                inputs = self.tokenizer(prompt, truncation=True, max_length=self.max_context_length, return_tensors="pt").to(self.device)
-                generation_output = self.model.generate(
-                        **inputs,
-                        max_new_tokens=self.max_new_tokens,
-                        num_beams=1,
-                        do_sample=False,
-                        temperature=1.0
-                    )
-                model_answer = self.tokenizer.decode(generation_output[0][-self.max_new_tokens:].cpu())
-            elif self.engine == 'vllm':
-                generation_output = self.model.generate(prompt, sampling_params=SamplingParams(temperature=0.0, max_tokens=self.max_new_tokens, use_beam_search=False, truncate_prompt_tokens=self.max_context_length))
-                model_answer = generation_output [0].outputs[0].text
-            else:
-                raise Exception('Engine should be \"hf\" or \"vllm\"')
-            
+            inputs = self.tokenizer(prompt, truncation=True, max_length=self.max_context_length, return_tensors="pt").to(self.device)
+            generation_output = self.model.generate(
+                    **inputs,
+                    max_new_tokens=self.max_new_tokens,
+                    num_beams=1,
+                    do_sample=False,
+                    temperature=1.0
+                )
+            model_answer = self.tokenizer.decode(generation_output[0][-self.max_new_tokens:].cpu())
             generated_answer = {
-                    "length": sample["length"],
-                    "model_answer": model_answer,
-                    "positive_outputs": sample["positive_outputs"],
-                    "negative_outputs": sample["negative_outputs"]
-                }
+                "length": sample["length"],
+                "model_answer": model_answer,
+                "positive_outputs": sample["positive_outputs"],
+                "negative_outputs": sample["negative_outputs"]
+            }
             generated_answers.append(generated_answer)
         return generated_answers
+
+class vLLM_AnswerGenerator(AnswerGenerator):
+    def generate_answers(self):
+
+        self.dataset = self.dataset.filter(lambda x: x['length'] in self.context_lengths)
+        self.dataset = self.dataset.map(self.create_prompt)
+        out = self.model.generate(self.dataset['prompt'], sampling_params=SamplingParams(temperature=0.0, max_tokens=self.max_new_tokens, use_beam_search=False, truncate_prompt_tokens=self.max_context_length))
+        
+        generated_answers = []
+
+        for ind, request in enumerate(out):
+            model_answer = request.outputs[0].text
+            generated_answer = {
+                        "length": self.dataset[ind]["length"],
+                        "model_answer": model_answer,
+                        "positive_outputs": self.dataset[ind]["positive_outputs"],
+                        "negative_outputs": self.dataset[ind]["negative_outputs"]
+                        }
+            generated_answers.append(generated_answer)
+        return generated_answers
+
+
